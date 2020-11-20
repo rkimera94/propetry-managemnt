579e25ba8722af56c5a532a675e0bc4439fd686d
Ronald Kimera <rkimera2013@gmail.com>
2020-11-20 20:58:36 +0300

    remove files

--- storage/framework/views/60ae6199da5d9efb208b4b3d3dc877a386d69361.php
+++
@@ -1,7 +0,0 @@
-<?php $__env->startSection('content'); ?>
-
-
-
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.admin', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/mac/Desktop/rkimera/laravel_project2/datascience/resources/views/contact.blade.php ENDPATH**/ ?>
